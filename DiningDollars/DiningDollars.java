c03b2176cc18dea0edd68afba06d83ecb07d8e2c
Christian Cheng <christiancheng516@gmail.com>
2016-03-03 19:52:51 -0800

    Convert prompts to constants

--- DiningDollars/DiningDollars.java
+++ DiningDollars/DiningDollars.java
@@ -11,10 +11,23 @@ import java.lang.*;
 
 public class DiningDollars {
 
+    // Constants for calculations
     private static final double TOTAL_AMOUNT = 3255;
     private static final double TOTAL_DAYS = 231;
     private static final double DAILY_ALLOWANCE = TOTAL_AMOUNT / TOTAL_DAYS;
     private static final double QUARTER_WEEKS = 11;
+    
+    // Prompts
+    private static final String PROMPT_ONE = "Please enter the current " +
+        "quarter (1 - Fall, 2 - Winter, 3 - Spring): ";
+    private static final String PROMPT_TWO = "Please enter the current " +
+        "Week (1-11): ";
+    private static final String PROMPT_THREE = "Please enter the day of " +
+        "the Week (1 (Sun) - 7 (Sat)): ";
+    private static final String PROMPT_FOUR = "Please enter your current " +
+        "Dining Dollars balance: ";
+    private static final String TOTAL_PROMPT = "\nYour current balance is $";
+    private static final String TARGET_PROMPT = "Your current target is $";
 
     public static void main(String[] args) {
 
@@ -28,22 +41,19 @@ public class DiningDollars {
         Scanner inputStream = new Scanner(System.in);
 
         // Prompt for current quarter
-        System.out.println("Please enter the current quarter (1 - Fall, 2 - "
-            + "Winter, 3 - Spring: ");
+        System.out.println(PROMPT_ONE);
         currentQuarter = inputStream.nextInt();
 
         // Prompt for current Week
-        System.out.println("Please enter the current Week (1-11): ");
+        System.out.println(PROMPT_TWO);
         currentWeek = inputStream.nextDouble();
 
         // Prompt for the day of the week
-        System.out.println("Please enter the day of the Week (1 (Sun) - 7 (" +
-                "Sat)): ");
+        System.out.println(PROMPT_THREE);
         day = inputStream.nextDouble();
         
         // Prompt for current Dining Dollars amount
-        System.out.println("Please enter your current Dining Dollars amount:" +
-                " ");
+        System.out.println(PROMPT_FOUR);
         currentTotal = inputStream.nextDouble();
 
         // Determine the current Week of the year (out of 33)
@@ -67,8 +77,8 @@ public class DiningDollars {
         buffer = (double)((int)buffer);
         buffer = buffer / 100;
 
-        System.out.println("\nYou are currently at $" + currentTotal);
-        System.out.println("Your current target is $" + currentTarget);
+        System.out.println(TOTAL_PROMPT + currentTotal);
+        System.out.println(TARGET_PROMPT + currentTarget);
       
         if (extra) { 
             System.out.println("You have $" + buffer + " extra Dining " +
